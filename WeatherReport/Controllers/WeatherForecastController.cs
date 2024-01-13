f89f9c4dbb99891719b8a80b27a6978130c61327
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2024-01-13 10:59:09 +0100

    Successfully configured appsettings.json and controller structure.

--- WeatherReport/Controllers/WeatherForecastController.cs
+++ WeatherReport/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
 using WeatherReport.Models;
 
 namespace WeatherReport.Controllers
@@ -9,20 +11,29 @@ namespace WeatherReport.Controllers
     {
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly HttpClient _httpClient;
+        private readonly WeatherApiSettings _apiSettings;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory, IOptions<WeatherApiSettings> apiSettings)
         {
             _logger = logger;  //DInjected. Used to log status of application.
+            _httpClient = httpClientFactory.CreateClient(); //DInjected httpClientFactory. Using factory design pattern to create object needed.
+            _apiSettings = apiSettings.Value; //DInjected object representing api settings from appsettings.json
         }
 
 
         /* Returns the Weather of a place in a determined time.
-         * GET call as .../WeatherAPI/GetWeather
+         * GET call as .../WeatherAPI/GetWeather.
+         * placeName: Defines a place. Could be the actual name of the place or latitude and longitude (e.g: 48.8567,2.3508).
+         * date: Defines a date. (yyyy-mm-dd)
+         * hour: Defines an hour. (0-23)
          */
         [HttpGet("GetWeather")]
-        public string GetWeather()
+        public async Task<IActionResult> GetWeather(string placeName, DateTime date, int hour)  //Return type is Task<IActionResult> and not IActionResult because the operation is async! Task handles mechanism of async such as concurrency.
         {
-            return "AAA";
+            string apiUrl = $"{_apiSettings.HistoryUrl}?key={_apiSettings.WeatherAPIKey}&q={placeName}&dt={date}&hour={hour}";
+            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+            return Ok(response);
         }
     }
 }
--- WeatherReport/Program.cs
+++ WeatherReport/Program.cs
@@ -1,10 +1,18 @@
 // Initializes a new instance of the WebApplication builder with preconfigured defaults.
+using WeatherReport;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the DI (Dependency Injection) container.
 
+//Adds possibility to do Http calls.
+builder.Services.AddHttpClient(); 
+
 // Adds MVC controllers to the services collection. 
 builder.Services.AddControllers();
+
+//Defining DI for class using appsettings.json.
+builder.Services.Configure<WeatherApiSettings>(builder.Configuration.GetSection("WeatherAPI"));
 
 // Adds API Explorer services which are necessary for generating Swagger (OpenAPI) documentation.
 // Swagger provides a UI to test your API endpoints and understand the request/response format.
